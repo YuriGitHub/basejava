12eb88ab6bd96f4244a23ac0ebaee6132bb0383a
Yuriy Perehudov <Yuriy.Perehudov@thinkmobiles.com>
2018-06-02 13:16:35 +0300

    added ArrayStorege implementation

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,57 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        this.storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        int length = this.size();
+        storage[length] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : this.storage){
+            if(resume.uuid.equals(uuid))
+                return resume;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int indexForDelete = -1;
+        for (int i = 0; i < storage.length; i++){
+            if(storage[i].uuid.equals(uuid)){
+                indexForDelete = i;
+                break;
+            }
+        }
+        if(indexForDelete != -1){
+            int lastIndex = size() - 1;
+            if(lastIndex >= 0){
+                Resume lastResume = storage[lastIndex];
+                storage[indexForDelete] = lastResume;
+                storage[lastIndex] = null;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] res = new Resume[size()];
+        System.arraycopy(storage, 0, res, 0, size());
+        return res;
     }
 
     int size() {
-        return 0;
+        int length = 0;
+        for (Resume aStorage : this.storage) {
+            if (aStorage == null)
+                break;
+            length++;
+        }
+        return length;
     }
 }
